24d69ad3c5a8d587f64d09815b5c9d9fde89f14b
Benjamin Khalife <31080657+parsgit@users.noreply.github.com>
2023-05-18 12:49:23 +0330

    Update Web.php

--- app/Routes/Web.php
+++ app/Routes/Web.php
@@ -3,5 +3,7 @@ use Webrium\Route;
 
 Route::get('','IndexController->index');
 
+Route::get('webhook/set','IndexController->setwebhook');
+Route::any('run','IndexController->run');
 
 Route::notFound();
